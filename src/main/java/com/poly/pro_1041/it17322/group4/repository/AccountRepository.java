b7f81f4ceadc2d7aea5c1360d71392795ec306dc
HUNGYEUHA\DELL <hungtqph26173@fpt.edu.vn>
2022-11-27 22:58:40 +0700

    ViewAccount

--- src/main/java/com/poly/pro_1041/it17322/group4/repository/AccountRepository.java
+++ src/main/java/com/poly/pro_1041/it17322/group4/repository/AccountRepository.java
@@ -6,18 +6,28 @@ package com.poly.pro_1041.it17322.group4.repository;
 
 import com.poly.pro_1041.it17322.group4.config.HibernateUtil;
 import com.poly.pro_1041.it17322.group4.domainmodel.Account;
+import java.util.List;
 import javax.persistence.Query;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 /**
  *
  * @author Huy PC
  */
 public class AccountRepository {
-    private Session session = HibernateUtil.getFACTORY().openSession();
-    
+
     private String fromTable = " FROM Account ";
-    public Account getOne(String username,String pass){
+
+    public List<Account> getAll() {
+        Session session = HibernateUtil.getFACTORY().openSession();
+        org.hibernate.query.Query query = session.createQuery(fromTable, Account.class);
+        List<Account> accs = query.getResultList();
+        return accs;
+    }
+
+    public Account getOne(String username, String pass) {
+        Session session = HibernateUtil.getFACTORY().openSession();
         String sql = fromTable + " WHERE Username=:User AND Password=:Pass";
         Query query = session.createQuery(sql, Account.class);
         query.setParameter("User", username);
@@ -25,6 +35,33 @@ public class AccountRepository {
         Account account = (Account) query.getSingleResult();
         return account;
     }
+
+    public Boolean add(Account acc) {
+        Transaction transaction = null;
+        try ( Session session = HibernateUtil.getFACTORY().openSession()) {
+            transaction = (Transaction) session.beginTransaction();
+            session.save(acc);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+        return null;
+    }
+
+    public Boolean update(Account acc) {
+        Transaction transaction = null;
+        try ( Session session = HibernateUtil.getFACTORY().openSession()) {
+            transaction = (Transaction) session.beginTransaction();
+            session.saveOrUpdate(acc);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
         System.out.println(new AccountRepository().getOne("nhanvien1", "12345678"));
     }
---
+++ src/main/java/com/poly/pro_1041/it17322/group4/service/impl/ViewAccountServiceImpl.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.poly.pro_1041.it17322.group4.service.impl;
+
+import com.poly.pro_1041.it17322.group4.domainmodel.Account;
+import com.poly.pro_1041.it17322.group4.repository.AccountRepository;
+import com.poly.pro_1041.it17322.group4.response.ViewAccountReponse;
+import com.poly.pro_1041.it17322.group4.service.ViewAccountService;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author DELL
+ */
+public class ViewAccountServiceImpl implements ViewAccountService {
+
+    private AccountRepository accrp = new AccountRepository();
+
+    @Override
+    public List<ViewAccountReponse> getAll() {
+        List<ViewAccountReponse> listAcc = new ArrayList<>();
+        List<Account> lists = accrp.getAll();
+        for (Account x : lists) {
+            listAcc.add(new ViewAccountReponse(x));
+        }
+        return listAcc;
+    }
+
+    public static void main(String[] args) {
+        List<Account> lists = new AccountRepository().getAll();
+        for (Account x : lists) {
+            System.out.println(x.toString());
+        }
+    }
+
+    @Override
+    public String add(ViewAccountReponse vaccr) {
+        Account acc = new Account(null, vaccr.getCvac(), vaccr.getTta(), vaccr.getHoTen(), vaccr.getNgaySinh(), vaccr.isGioiTinh(), vaccr.getSdt(), vaccr.getDiaChi(), vaccr.getEmail(), vaccr.getUsername(), vaccr.getPassword(), vaccr.getNgayTao(), "B15213A1-0B8C-41BF-B6AE-B9FCD2322F41", null, null);
+        boolean add = accrp.add(acc);
+        if (add) {
+            return "Thêm thành công!";
+        } else {
+            return "Thêm lỗi!";
+        }
+    }
+
+    @Override
+    public String update(ViewAccountReponse vaccr) {
+        Account acc = new Account(vaccr.getId(), vaccr.getCvac(), vaccr.getTta(), vaccr.getHoTen(), vaccr.getNgaySinh(), vaccr.isGioiTinh(), vaccr.getSdt(), vaccr.getDiaChi(), vaccr.getEmail(), vaccr.getUsername(), vaccr.getPassword(), vaccr.getNgayTao(), "B15213A1-0B8C-41BF-B6AE-B9FCD2322F41", null, null);
+        boolean update = accrp.update(acc);
+        if (update) {
+            return "Sửa thành công!";
+        } else {
+            return "Sửa lỗi!";
+        }
+    }
+
+}
